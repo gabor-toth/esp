83ff960d56746556117809ef179073d464131985
Gabor Toth <gabor.toth@p92.hu>
2022-06-01 22:27:13 +0200

    add fontawesome

--- ontozo/frontend/src/app/app.component.ts
+++ ontozo/frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+// see https://fontawesome.com/icons/square-caret-down?s=solid
+import {faSquareCaretDown, faSquareCaretUp, faToggleOff, faToggleOn} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
+    faSquareCaretDown = faSquareCaretDown;
+    faSquareCaretUp = faSquareCaretUp;
+    faToggleOff = faToggleOff
+    faToggleOn = faToggleOn
 }
