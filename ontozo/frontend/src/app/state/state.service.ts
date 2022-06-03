f91ddfb243cc48c491eded51bbe7f048df853f60
Gabor Toth <gabor.toth@p92.hu>
2022-06-04 00:57:40 +0200

    proper baseUrl on prod

--- ontozo/frontend/src/app/state/state.service.ts
+++ ontozo/frontend/src/app/state/state.service.ts
@@ -2,23 +2,22 @@ import {Injectable} from '@angular/core';
 import {State} from './state';
 import {Observable} from "rxjs";
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StateService {
 
-    baseUrl = 'http://192.168.1.82/';
-
     constructor(private http: HttpClient) {
     }
 
     getState(): Observable<State> {
-        return this.http.get<State>(this.baseUrl + 'state');
+        return this.http.get<State>(environment.baseUrl + 'state');
     }
 
     setState(type: String, id: number, state: boolean): Observable<Object> {
-        let url = this.baseUrl + type;
+        let url = environment.baseUrl + type;
         let body = `{"id":${id}, "state": ${state}}`;
         return this.http.put(url, body);
     }
