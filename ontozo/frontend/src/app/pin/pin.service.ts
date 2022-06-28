9235586f5764ccef4203e3d268b4b75c8a452d90
Gabor Toth <gabor.toth@p92.hu>
2022-06-28 21:10:32 +0200

    move out pin state stuff

---
+++ ontozo/frontend/src/app/pin/pin.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {PinsState} from './pin';
+import {Observable} from "rxjs";
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PinService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getState(): Observable<PinsState> {
+        return this.http.get<PinsState>(environment.baseUrl + 'state');
+    }
+
+    setState(type: String, id: number, state: boolean): Observable<Object> {
+        let url = environment.baseUrl + type;
+        let body = `{"id":${id}, "state": ${state}}`;
+        return this.http.put(url, body);
+    }
+}
---
+++ ontozo/frontend/src/app/pin/pin.ts
@@ -0,0 +1,26 @@
+export interface PinState {
+    id: number;
+    name: string;
+    on: boolean;
+}
+
+export interface PinInputsState {
+    levels: PinState[];
+    buttons: PinState[];
+}
+
+export interface PinOutputsState {
+    zones: PinState[];
+    pumps: PinState[];
+}
+
+export interface TimeState {
+    time?: String;
+    isTimeSet: boolean;
+}
+
+export interface PinsState {
+    inputs: PinInputsState;
+    outputs: PinOutputsState;
+    time?: TimeState | null;
+}
