578305306b2e6ac0cb10e3516429fbadd2618b83
Gabor Toth <gabor.toth@p92.hu>
2023-04-16 22:26:40 +0200

    comment back adc init

--- hajo/main/hajo_adc.c
+++ hajo/main/hajo_adc.c
@@ -4,9 +4,7 @@
 
 static int battery_rmes = 16900;
 static int battery_rtop = 316000;
-static int battery_offset = 150;
-//static int battery_rmes = 16900;
-//static int battery_rtop = 316000;
+static int battery_offset = 0;
 static double battery_multiplier;
 
 static double fluid_u = 3.32;
@@ -41,7 +39,7 @@ void convert_fluid_level( uint32_t raw_value, uint32_t *display_value, uint32_t 
     } else if ( rmes >= fluid_rmes_max ) {
         value = 100;
     } else {
-        value = ( uint32_t )(( rmes - fluid_rmes_min ) / ( fluid_rmes_max - fluid_rmes_min ) * 100 );
+        value = (uint32_t) (( rmes - fluid_rmes_min ) / ( fluid_rmes_max - fluid_rmes_min ) * 100 );
     }
     *display_value = value;
     *correction = 0;
@@ -58,5 +56,5 @@ void hajo_adc_main( bool is_battery ) {
         adc_add_channel( 5, "viz bal", 0, N2K_TANK_TYPE_WATER, convert_fluid_level );
         adc_add_channel( 6, "viz jobb", 1, N2K_TANK_TYPE_WATER, convert_fluid_level );
     }
-//    adc_main();
+    adc_main( false );
 }
