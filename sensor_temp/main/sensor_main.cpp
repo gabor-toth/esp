50b210ad72d3db275f1425142b8cc4078b5f0581
Gábor Tóth <gabor.toth@softvar.hu>
2024-01-04 00:22:28 +0100

    implement main logic

---
+++ sensor_temp/main/sensor_main.cpp
@@ -0,0 +1,39 @@
+#include "config.h"
+#include "ds18b20.h"
+#include "driver/gpio.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_main.h"
+#include "owb_gpio.h"
+
+static const char *TAG = "sensor_main";
+
+static void led_off() {
+    gpio_set_direction(GPIO_NUM_15, GPIO_MODE_OUTPUT );
+    gpio_set_level(GPIO_NUM_15, 0 );
+}
+
+void sensor_main() {
+    nvs_init();
+
+    ESP_ERROR_CHECK( esp_event_loop_create_default());
+
+    led_off();
+    owb_gpio_driver_info driver_info;
+    OneWireBus* bus = owb_gpio_initialize(&driver_info, GPIO_NUM_14 );
+    DS18B20_Info* device = ds18b20_malloc();
+    ds18b20_init_solo(device, bus);
+    ds18b20_set_resolution(device, DS18B20_RESOLUTION_12_BIT );
+    if ( ds18b20_convert(device) ) {
+        ds18b20_wait_for_conversion(device);
+        float temp;
+        DS18B20_ERROR result = ds18b20_read_temp(device, &temp);
+        ESP_LOGI(TAG, "result %d %f", result, temp);
+    }
+}
+
+extern "C" {
+void app_main() {
+    sensor_main();
+}
+}
