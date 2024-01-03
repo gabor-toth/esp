76f4ef1b7ccbd620c60045904d88bb5e775c1038
Gábor Tóth <gabor.toth@softvar.hu>
2024-01-04 00:18:55 +0100

    project started

---
+++ sensor_temp/init.sh
@@ -0,0 +1,11 @@
+if [ $(basename $0) == "init.sh" ]; then
+  echo "Source this file with '. ./$(basename $0)'"
+  exit
+fi
+
+base_dir=$(readlink -f $(dirname ${BASH_SOURCE[0]})/..)
+
+. $base_dir/../esp-idf/export.sh
+
+echo "Set up git credentials"
+export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_gabtoth -o IdentitiesOnly=yes"
---
+++ sensor_temp/main/config.h
@@ -0,0 +1,4 @@
+#ifndef HAJO_CONFIG_H
+#define HAJO_CONFIG_H
+
+#endif //HAJO_CONFIG_H
