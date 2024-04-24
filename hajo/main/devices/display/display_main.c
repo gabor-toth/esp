ad7691fe873682015429881224e1696900e299a3
Gábor Tóth <gabor.toth@softvar.hu>
2024-04-24 23:51:22 +0200

    new design

--- hajo/main/devices/display/display_main.c
+++ hajo/main/devices/display/display_main.c
@@ -1,13 +1,13 @@
 #include "display_main.h"
 #include "display_meter.h"
-#include "esp_lcd_backlight.h"
 #include "esp_log.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
-#include "lvgl_esp32_drivers/lvgl_helpers.h"
+//#include "esp_lcd_backlight.h"
+#include "lvgl/lvgl.h"
 #include <stdlib.h>
 
 /*********************
@@ -33,7 +33,7 @@ static void backlight_callback( TimerHandle_t timer );
 static const char *LOG = "display";
 
 static SemaphoreHandle_t xGuiSemaphore;
-static disp_backlight_config_t *backlight_handler;
+//static disp_backlight_config_t *backlight_handler;
 static int backlight_off_interval = 5;
 static TimerHandle_t backlight_timer;
 static bool is_display_on;
@@ -71,23 +71,23 @@ void display_end_task() {
  **********************/
 
 static void backlight_on() {
-    disp_backlight_set( backlight_handler, 100 );
+//    disp_backlight_set( backlight_handler, 100 );
     is_display_on = true;
     xTimerStart( backlight_timer, portMAX_DELAY );
     ESP_LOGI( LOG, "backlight_on" );
 }
 
 static void backlight_off() {
-    disp_backlight_set( backlight_handler, 0 );
+//    disp_backlight_set( backlight_handler, 0 );
     is_display_on = false;
     xTimerStop( backlight_timer, portMAX_DELAY );
     ESP_LOGI( LOG, "backlight_off" );
 }
 
-static void indev_read( lv_indev_drv_t *drv, lv_indev_data_t *data ) {
-    touch_driver_read( drv, data );
+static void indev_read( lv_indev_t *indev, lv_indev_data_t *data ) {
+//    touch_driver_read( drv, data );
     
-    lv_indev_state_t current_state = indev->proc.state;
+    lv_indev_state_t current_state = lv_indev_get_state( indev );
     if ( indev_previous_state != current_state ) {
         ESP_LOGI( LOG, "current_state %d, previous_state %d, display_on %d",
                   current_state, indev_previous_state, is_display_on );
@@ -107,24 +107,19 @@ static void guiTask( void *pvParameter ) {
     lv_init();
     
     /* Initialize SPI or I2C bus used by the drivers */
-    backlight_handler = lvgl_driver_init();
-    backlight_on();
+//    backlight_handler = lvgl_driver_init();
+//    backlight_on();
     
-    uint32_t size_in_px = DISP_BUF_SIZE;
-    
-    lv_color_t * buf1 = heap_caps_malloc( size_in_px * sizeof( lv_color_t ), MALLOC_CAP_DMA );
+    uint32_t size_in_px = CONFIG_LV_DRAW_LAYER_SIMPLE_BUF_SIZE;
+    uint32_t size_in_bytes = size_in_px * sizeof( lv_color_t );
+    lv_color_t *buf1 = heap_caps_malloc( size_in_bytes, MALLOC_CAP_DMA );
     assert( buf1 != NULL );
-    lv_color_t * buf2 = heap_caps_malloc( size_in_px * sizeof( lv_color_t ), MALLOC_CAP_DMA );
+    lv_color_t *buf2 = heap_caps_malloc( size_in_bytes, MALLOC_CAP_DMA );
     assert( buf2 != NULL );
     
-    static lv_disp_draw_buf_t disp_buf;
-    lv_disp_draw_buf_init( &disp_buf, buf1, buf2, size_in_px );
-    
-    lv_disp_drv_t disp_drv;
-    lv_disp_drv_init( &disp_drv );
-    disp_drv.flush_cb = disp_driver_flush;
-    disp_drv.draw_buf = &disp_buf;
-    lv_disp_drv_register( &disp_drv );
+    lv_display_t *disp = lv_display_create( 240, 320 );
+//    lv_display_set_flush_cb( disp, disp_driver_flush );
+    lv_display_set_buffers( disp, buf1, buf2, size_in_bytes, LV_DISPLAY_RENDER_MODE_PARTIAL );
     
     /* Create and start a periodic timer interrupt to call lv_tick_inc */
     const esp_timer_create_args_t periodic_timer_args = {
@@ -135,14 +130,11 @@ static void guiTask( void *pvParameter ) {
     ESP_ERROR_CHECK( esp_timer_create( &periodic_timer_args, &periodic_timer ) );
     ESP_ERROR_CHECK( esp_timer_start_periodic( periodic_timer, LV_TICK_PERIOD_MS * 1000 ) );
     
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init( &indev_drv );
-    indev_drv.read_cb = indev_read;
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev = lv_indev_drv_register( &indev_drv );
-
-//    display_meter_main_single();
-    display_meter_main_tabbed();
+    indev = lv_indev_create();
+    lv_indev_set_type( indev, LV_INDEV_TYPE_POINTER );
+    lv_indev_set_read_cb( indev, indev_read );
+    
+    display_meter_main();
     
     while ( 1 ) {
         /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
@@ -150,15 +142,14 @@ static void guiTask( void *pvParameter ) {
         
         /* Try to take the semaphore, call lvgl related function on success */
         if ( display_start_task() ) {
-            lv_task_handler();
+            lv_timer_handler();
             display_end_task();
         }
     }
 }
 
 void display_set_value( display_type_t type, int instance, int value ) {
-//    display_set_value_single( type, instance, value );
-    display_set_value_tabbed( type, instance, value );
+    display_meter_set_value( type, instance, value );
 }
 
 static void lv_tick_task( void *arg ) {
--- hajo/main/devices/display/display_meter.h
+++ hajo/main/devices/display/display_meter.h
@@ -7,11 +7,9 @@ extern "C" {
 
 #include "display_common.h"
 
-extern void display_meter_main_tabbed();
-extern void display_set_value_tabbed( display_type_t type, int instance, int value );
+extern void display_meter_main();
 
-extern void display_meter_main_single();
-extern void display_set_value_single( display_type_t type, int instance, int value );
+extern void display_meter_set_value( display_type_t type, int instance, int value );
 
 #ifdef __cplusplus
 }
