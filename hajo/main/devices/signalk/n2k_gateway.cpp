6be496901589c55b285940265f6ecefddef824d1
Gábor Tóth <gabor.toth@softvar.hu>
2024-07-24 00:43:17 +0200

    make mem log configurable

--- hajo/main/devices/signalk/n2k_gateway.cpp
+++ hajo/main/devices/signalk/n2k_gateway.cpp
@@ -1,17 +1,27 @@
+#include "debug_helper.h"
+#include "esp_timer.h"
 #include "n2k_gateway.h"
+#include "n2k_gateway_actisense.h"
+#include "n2k_gateway_wifi.h"
 #include "n2k_simulator.h"
 #include "sdkconfig.h"
 
-#if CONFIG_SIGNALK_OVER_ACTISENSE
+void debug_timer_cb( void *arg ) {
+    debug_print_free_mem( nullptr );
+}
 
-#include "n2k_gateway_actisense.h"
-
-#endif
-#if CONFIG_SIGNALK_OVER_WIFI
-
-#include "n2k_gateway_wifi.h"
-
-#endif
+void start_free_mem_timer() {
+    esp_timer_create_args_t timer_args = {
+            .callback = debug_timer_cb,
+            .arg = nullptr,
+            .dispatch_method= ESP_TIMER_TASK,
+            .name = nullptr,
+            .skip_unhandled_events= true,
+    };
+    esp_timer_handle_t timer_handle = nullptr;
+    ESP_ERROR_CHECK( esp_timer_create( &timer_args, &timer_handle ) );
+    ESP_ERROR_CHECK( esp_timer_start_periodic( timer_handle, 1000L * 1000L ) );
+}
 
 void sendN2KMessageToSignalK( const tN2kMsg &N2kMsg ) {
 #if CONFIG_SIGNALK_OVER_ACTISENSE
@@ -23,6 +33,9 @@ void sendN2KMessageToSignalK( const tN2kMsg &N2kMsg ) {
 }
 
 void setupSignalkChannels() {
+#if CONFIG_SIGNALK_LOG_FREE_MEM
+    start_free_mem_timer();
+#endif
 #if CONFIG_SIGNALK_OVER_ACTISENSE
     setupSignalkOverActisense();
 #endif
