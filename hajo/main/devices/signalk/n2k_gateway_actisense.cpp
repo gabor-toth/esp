d617ea7f1cf30cc93eec6df5b5c2336d7af80427
Gábor Tóth <gabor.toth@softvar.hu>
2024-12-04 20:38:39 +0100

    gps week rollover fix

--- hajo/main/devices/signalk/n2k_gateway_actisense.cpp
+++ hajo/main/devices/signalk/n2k_gateway_actisense.cpp
@@ -3,7 +3,10 @@
 #include "driver/uart.h"
 #include "esp_log.h"
 #include "n2k_gateway_actisense.h"
+#include "N2kMessages.h"
+#include "N2kTypes.h"
 #include "sdkconfig.h"
+#include <ctime>
 
 #if CONFIG_SIGNALK_OVER_ACTISENSE
 
@@ -27,8 +30,76 @@ class SerialN2kStream : public N2kStream {
 
 SerialN2kStream serialN2kStream = {};
 
+void correctGpsWeekIfNeeded( uint16_t& since1970 ) {
+    // https://en.wikipedia.org/wiki/GPS_week_number_rollover
+    // The second rollover occurred on the night of April 6 to 7, 2019
+    long lastRollOverDate = 1554588000l / 24 / 60 / 60;
+    while ( since1970 < lastRollOverDate ) {
+        since1970 += 1024 * 7; // add 1024 weeks
+    }
+}
+
+const tN2kMsg*  HandleTimeAndDate( const tN2kMsg &N2kMsg , tN2kMsg& newN2kMsg ) {
+    uint16_t DaysSince1970;
+    double SecondsSinceMidnight;
+    int16_t LocalOffset;
+
+    if (ParseN2kPGN129033( N2kMsg,DaysSince1970, SecondsSinceMidnight,LocalOffset) ) {
+        correctGpsWeekIfNeeded(DaysSince1970);
+        SetN2kPGN129033(newN2kMsg, DaysSince1970, SecondsSinceMidnight, LocalOffset);
+        return &newN2kMsg;
+    }
+    return &N2kMsg;
+}
+
+const tN2kMsg* HandleGNSS( const tN2kMsg& N2kMsg, tN2kMsg& newN2kMsg )  {
+
+    unsigned char SID;
+    uint16_t DaysSince1970;
+    double SecondsSinceMidnight;
+    double Latitude;
+    double Longitude;
+    double Altitude;
+    tN2kGNSStype GNSSType;
+    tN2kGNSSmethod GNSSMethod;
+    unsigned char nSatellites;
+    double HDOP;
+    double PDOP;
+    double GeoidalSeparation;
+    unsigned char nReferenceStations;
+    tN2kGNSStype ReferenceStationType;
+    uint16_t ReferenceStationID;
+    double AgeOfCorrection;
+
+    if ( ParseN2kGNSS( N2kMsg, SID, DaysSince1970, SecondsSinceMidnight, Latitude, Longitude, Altitude, GNSSType,
+                       GNSSMethod,
+                       nSatellites, HDOP, PDOP, GeoidalSeparation,
+                       nReferenceStations, ReferenceStationType, ReferenceStationID, AgeOfCorrection )) {
+        correctGpsWeekIfNeeded(DaysSince1970);
+        SetN2kPGN129029(newN2kMsg, SID, DaysSince1970, SecondsSinceMidnight, Latitude, Longitude, Altitude, GNSSType,
+                        GNSSMethod,N2kGNSSi_noIntegrityChecking,
+                        nSatellites, HDOP, PDOP, GeoidalSeparation,
+                        nReferenceStations, ReferenceStationType, ReferenceStationID, AgeOfCorrection);
+        return &newN2kMsg;
+    }
+    return &N2kMsg;
+}
+
 void sendN2KMessageToSignalKOverActisense( const tN2kMsg &N2kMsg ) {
-    N2kMsg.SendInActisenseFormat( &serialN2kStream );
+    tN2kMsg newN2kMsg;
+    const tN2kMsg* N2kMsgToSend;
+    switch ( N2kMsg.PGN ) {
+        case 129029L:
+            N2kMsgToSend = HandleGNSS( N2kMsg, newN2kMsg );
+            break;
+        case 129033L:
+            N2kMsgToSend = HandleTimeAndDate( N2kMsg , newN2kMsg );
+            break;
+        default:
+            N2kMsgToSend = &N2kMsg;
+            break;
+    }
+    N2kMsgToSend->SendInActisenseFormat( &serialN2kStream );
 }
 
 void setupSignalkOverActisense() {
