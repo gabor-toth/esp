56bf41856f2989f473eb6cf68cdfe1b3d4bc3395
Gábor Tóth <gabor.toth@softvar.hu>
2025-08-11 00:35:54 +0200

    upgrade ontozo to idf 5.4.0

--- common/wifi/scan.c
+++ common/wifi/scan.c
@@ -18,6 +18,7 @@
 #include "esp_log.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
+#include "regex.h"
 
 // OWN start
 #include "wifi_main.h"
@@ -38,6 +39,12 @@ typedef struct {
 
 /* OWN commented out
 #define DEFAULT_SCAN_LIST_SIZE CONFIG_EXAMPLE_SCAN_LIST_SIZE
+
+#ifdef CONFIG_EXAMPLE_USE_SCAN_CHANNEL_BITMAP
+#define USE_CHANNEL_BITMAP 1
+#define CHANNEL_LIST_SIZE 3
+static uint8_t channel_list[CHANNEL_LIST_SIZE] = {1, 6, 11};
+#endif / *CONFIG_EXAMPLE_USE_SCAN_CHANNEL_BITMAP* /
 
 static const char *TAG = "scan";
 
@@ -71,14 +78,14 @@ static void print_auth_mode(int authmode)
     case WIFI_AUTH_WPA2_WPA3_PSK:
         ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_WPA3_PSK");
         break;
+    case WIFI_AUTH_WPA3_ENTERPRISE:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA3_ENTERPRISE");
+        break;
+    case WIFI_AUTH_WPA2_WPA3_ENTERPRISE:
+        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA2_WPA3_ENTERPRISE");
+        break;
     case WIFI_AUTH_WPA3_ENT_192:
         ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA3_ENT_192");
-        break;
-    case WIFI_AUTH_WPA3_EXT_PSK:
-        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA3_EXT_PSK");
-        break;
-    case WIFI_AUTH_WPA3_EXT_PSK_MIXED_MODE:
-        ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_WPA3_EXT_PSK_MIXED_MODE");
         break;
     default:
         ESP_LOGI(TAG, "Authmode \tWIFI_AUTH_UNKNOWN");
@@ -157,8 +164,19 @@ static void print_cipher_type(int pairwise_cipher, int group_cipher)
         break;
     }
 }
+
+#ifdef USE_CHANNEL_BITMAP
+static void array_2_channel_bitmap(const uint8_t channel_list[], const uint8_t channel_list_size, wifi_scan_config_t *scan_config) {
+
+    for(uint8_t i = 0; i < channel_list_size; i++) {
+        uint8_t channel = channel_list[i];
+        scan_config->channel_bitmap.ghz_2_channels |= (1 << channel);
+    }
+}
+#endif / *USE_CHANNEL_BITMAP* /
 */
 
+// OWN start
 _Noreturn static void task_wifi_scan( void *arg ) {
     for ( ;; ) {
         event_callback_parameters parameters;
@@ -178,11 +196,16 @@ static void on_wifi_scanned(void *sta_netif, esp_event_base_t event_base,
     };
     xQueueSend(evt_queue , &parameters, portMAX_DELAY);
 }
+// OWN end
 
 /* Initialize Wi-Fi as sta and set scan method */
 void wifi_scan(void) {
-    evt_queue = xQueueCreate( 10, sizeof( event_callback_parameters ) );
-    xTaskCreate( task_wifi_scan, TAG, 4096, NULL, 10, NULL );
+    // OWN start
+    if ( evt_queue == NULL ) {
+        evt_queue = xQueueCreate( 10, sizeof( event_callback_parameters ) );
+        xTaskCreate( task_wifi_scan, TAG, 4096, NULL, 10, NULL );
+    }
+    // OWN end
 
     ESP_ERROR_CHECK(esp_netif_init());
     // OWN commented out
@@ -197,6 +220,7 @@ void wifi_scan(void) {
 #if ASYNC_WIFI_INIT
     ESP_ERROR_CHECK( esp_wifi_set_mode( WIFI_MODE_STA ));
     // will be deregistered in wifi_handler_on_scan_done
+    // OWN modified to on_wifi_scanned
     ESP_ERROR_CHECK( esp_event_handler_register( WIFI_EVENT, WIFI_EVENT_SCAN_DONE, &on_wifi_scanned,
                                                  sta_netif ));
     ESP_ERROR_CHECK( esp_wifi_start());
@@ -207,14 +231,28 @@ void wifi_scan(void) {
     uint16_t number = DEFAULT_SCAN_LIST_SIZE;
     wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
     uint16_t ap_count = 0;
-    memset( ap_info, 0, sizeof(ap_info));
+    memset(ap_info, 0, sizeof(ap_info));
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
+
+#ifdef USE_CHANNEL_BITMAP
+    wifi_scan_config_t *scan_config = (wifi_scan_config_t *)calloc(1,sizeof(wifi_scan_config_t));
+    if (!scan_config) {
+        ESP_LOGE(TAG, "Memory Allocation for scan config failed!");
+        return;
+    }
+    array_2_channel_bitmap(channel_list, CHANNEL_LIST_SIZE, scan_config);
+    esp_wifi_scan_start(scan_config, true);
+    free(scan_config);
+
+#else
     esp_wifi_scan_start(NULL, true);
+#endif /*USE_CHANNEL_BITMAP*/
+
     ESP_LOGI(TAG, "Max AP number ap_info can hold = %u", number);
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
     ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
     ESP_LOGI(TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
     for (int i = 0; i < number; i++) {
         ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
